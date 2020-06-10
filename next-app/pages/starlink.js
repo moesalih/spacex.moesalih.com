2afa60fe954e8f959882978d8a7383b3102269a4
Moe Salih <moe.salih@gmail.com>
2020-06-10 09:48:25 -0400

    Starlink - small tweaks

--- next-app/pages/starlink.js
+++ next-app/pages/starlink.js
@@ -211,6 +211,7 @@ export default class Starlink extends React.Component {
 								<p>This chart show the current oribital parameters of each Starlink satellite launched so far. Please note that some satellites have different parameter timestamps so they might not line up exactly with others.</p>
 								<p><strong>Anomaly past Ascending Node</strong> refers to the position of each satellite in its plane, and is the sum of <a href="https://en.wikipedia.org/wiki/Argument_of_periapsis" target="_blank">Argument of perigee</a> and <a href="https://en.wikipedia.org/wiki/Mean_anomaly" target="_blank">Mean anomoly</a>.</p>
 								<p><a href="https://en.wikipedia.org/wiki/Longitude_of_the_ascending_node" target="_blank"><strong>Longitude of Ascending Node</strong></a> refers to the orientation of each satellite's plane.</p>
+								<p>Altitude is represented by the opacity of each point (300 - 550 KM)</p>
 								<p>All Starlink satellites have the same inclination of 53°</p>
 
 								<p>More info here: <a href="https://en.wikipedia.org/wiki/Orbital_elements" target="_blank">Orbital Elements (Wikipedia)</a></p>
