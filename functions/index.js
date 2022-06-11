47b9b7f5e839ad59dd5f78a98c7eff846a25197f
Moe Salih <moe.salih@gmail.com>
2022-06-11 11:18:54 -0400

    Handle split of lanches on wikipedia to two pages

--- functions/index.js
+++ functions/index.js
@@ -106,96 +106,33 @@ exports.launchesCal = functions.https.onRequest(async (request, response) => {
 	}
 })
 
+exports.testLaunches = functions.https.onRequest(async (request, response) => {
+	try {
+		let data = await getLaunches()
+		response.json(data)
+	} catch (e) {
+		console.error(e)
+		response.json({ error: e })
+	}
+})
+
 
 async function getLaunches() {
 	try {
 
-		let response = await axios({ url: 'https://en.wikipedia.org/wiki/List_of_Falcon_9_and_Falcon_Heavy_launches', timeout: 5000 })
-		if (!response.data) { throw null }
 
-		var $ = cheerio.load(response.data)
+		var old = await loadPage('https://en.wikipedia.org/wiki/List_of_Falcon_9_and_Falcon_Heavy_launches_(2010%E2%80%932019)')
+		var current = await loadPage('https://en.wikipedia.org/wiki/List_of_Falcon_9_and_Falcon_Heavy_launches')
 		
-		var futureLaunchesH2 = $("#Future_launches").parent()
-		var futureLaunchesTable = futureLaunchesH2.nextAll('table')
-
-		var rows = futureLaunchesTable.find("tr")
-		rows = rows.filter(function (i, el) {
-			if ($(this).find("th").length > 0) return false // hide header
-			if ($(this).find("td").first().attr("colspan") == 6) return false // hide year rows
-			return true
-		})
-
 		var data = {
-			launches: []
+			pastLaunches: [
+				...getRows(old, '#Launches', 'table.collapsible', parsePastRows),
+				...getRows(current, '#Past_launches', 'table.collapsible', parsePastRows),
+			],
+			launches: [
+				...getRows(current, '#Future_launches', 'table', parseFutureRows),
+			],
 		}
-
-		var launch = {}
-		rows.each(function (i, el) {
-			$(this).find('br').replaceWith(' ')
-			var children = $(this).children()
-			// console.log(children.length)
-			if (children.first().attr("rowspan")) {
-				launch = {}
-				launch.dateText = removeReferences(children.eq(0).text())
-				launch.dateText = launch.dateText.replace(/(\d\d:\d\d)/, ' $1')
-				if (launch.dateText.match(/(\d\d:\d\d)/)) launch.date = new Date(launch.dateText + ' UTC')
-				if (isNaN(launch.date)) launch.date = null
-				launch.type = removeReferences(children.eq(1).text())
-				launch.site = removeReferences(children.eq(2).text())
-				launch.payload = removeReferences(children.eq(3).text())
-				launch.payloadIcon = getPayloadIcon(launch.payload)
-				launch.orbit = removeReferences(children.eq(4).text())
-				launch.customer = removeReferences(children.eq(5).text())
-			}
-			else if (!children.first().attr("colspan") && children.length == 1) {
-				launch.type += ', ' + removeReferences(children.eq(0).text())
-			}
-			else if (children.first().attr("colspan")) {
-				launch.note = removeReferences(children.eq(0).text())
-				launch.payloadIcon = launch.payloadIcon || getPayloadIcon(launch.note)
-				data.launches.push(launch)
-			}
-		})
-
-		
-		var pastLaunchesH2 = $("#Past_launches").parent()
-		var pastLaunchesTable = pastLaunchesH2.nextAll('table.collapsible')
-
-		var rows = pastLaunchesTable.find("tr")
-		rows = rows.filter(function (i, el) {
-			if ($(this).find("th").length > 2) return false // hide header
-			// if ($(this).find("td").first().attr("colspan") == 6) return false // hide year rows
-			return true
-		})
-		data.pastLaunches = []
-		var launch = {}
-		rows.each(function (i, el) {
-			$(this).find('br').replaceWith(' ')
-			var children = $(this).children()
-			// console.log(children.length)
-			if (children.first().attr("rowspan")) {
-				launch = {}
-				launch.dateText = removeReferences(children.eq(1).text())
-				launch.dateText = launch.dateText.replace(/(\d\d:\d\d)/, ' $1')
-				if (launch.dateText.match(/(\d\d:\d\d)/)) launch.date = new Date(launch.dateText + ' UTC')
-				if (isNaN(launch.date)) launch.date = null
-				launch.type = removeReferences(children.eq(2).text())
-				launch.site = removeReferences(children.eq(3).text())
-				launch.payload = removeReferences(children.eq(4).text())
-				launch.payloadIcon = getPayloadIcon(launch.payload)
-				launch.orbit = removeReferences(children.eq(6).text())
-				launch.customer = removeReferences(children.eq(7).text())
-				launch.outcome = removeReferences(children.eq(8).text())
-			}
-			else if (!children.first().attr("colspan") && children.length == 2) {
-				launch.type += ', ' + removeReferences(children.eq(0).text())
-			}
-			else if (children.first().attr("colspan")) {
-				launch.note = removeReferences(children.eq(0).text())
-				launch.payloadIcon = launch.payloadIcon || getPayloadIcon(launch.note)
-				data.pastLaunches.push(launch)
-			}
-		})
 
 		// console.log(data)
 		return data
@@ -204,6 +141,93 @@ async function getLaunches() {
 		console.error(e);
 		return null
 	}
+}
+
+async function loadPage(url) {
+	let response = await axios({ url, timeout: 5000 })
+	if (!response.data) { throw null }
+
+	var $ = cheerio.load(response.data)
+	return $
+}
+
+function getRows($, h2Selector, tableSelector, parseFunc) {
+	var launchesH2 = $(h2Selector).parent()
+	var launchesTable = launchesH2.nextAll(tableSelector)
+
+	var rows = launchesTable.find("tr")
+	rows = rows.filter(function (i, el) {
+		if ($(this).find("th").length > 2) return false // hide header
+		// if ($(this).find("td").first().attr("colspan") == 6) return false // hide year rows
+		return true
+	})
+
+	let launches = parseFunc(rows, $)
+	return launches
+}
+
+function parseFutureRows(rows, $) {
+	var launches = []
+	var launch = {}
+	rows.each(function (i, el) {
+		$(this).find('br').replaceWith(' ')
+		var children = $(this).children()
+		// console.log(children.length)
+		if (children.first().attr("rowspan")) {
+			launch = {}
+			launch.dateText = removeReferences(children.eq(0).text())
+			launch.dateText = launch.dateText.replace(/(\d\d:\d\d)/, ' $1')
+			if (launch.dateText.match(/(\d\d:\d\d)/)) launch.date = new Date(launch.dateText + ' UTC')
+			if (isNaN(launch.date)) launch.date = null
+			launch.type = removeReferences(children.eq(1).text())
+			launch.site = removeReferences(children.eq(2).text())
+			launch.payload = removeReferences(children.eq(3).text())
+			launch.payloadIcon = getPayloadIcon(launch.payload)
+			launch.orbit = removeReferences(children.eq(4).text())
+			launch.customer = removeReferences(children.eq(5).text())
+		}
+		else if (!children.first().attr("colspan") && children.length == 1) {
+			launch.type += ', ' + removeReferences(children.eq(0).text())
+		}
+		else if (children.first().attr("colspan")) {
+			launch.note = removeReferences(children.eq(0).text())
+			launch.payloadIcon = launch.payloadIcon || getPayloadIcon(launch.note)
+			launches.push(launch)
+		}
+	})
+	return launches
+}
+function parsePastRows(rows, $) {
+	var launches = []
+	var launch = {}
+	rows.each(function (i, el) {
+		$(this).find('br').replaceWith(' ')
+		var children = $(this).children()
+		// console.log(children.length)
+		if (children.first().attr("rowspan")) {
+			launch = {}
+			launch.dateText = removeReferences(children.eq(1).text())
+			launch.dateText = launch.dateText.replace(/(\d\d:\d\d)/, ' $1')
+			if (launch.dateText.match(/(\d\d:\d\d)/)) launch.date = new Date(launch.dateText + ' UTC')
+			if (isNaN(launch.date)) launch.date = null
+			launch.type = removeReferences(children.eq(2).text())
+			launch.site = removeReferences(children.eq(3).text())
+			launch.payload = removeReferences(children.eq(4).text())
+			launch.payloadIcon = getPayloadIcon(launch.payload)
+			launch.orbit = removeReferences(children.eq(6).text())
+			launch.customer = removeReferences(children.eq(7).text())
+			launch.outcome = removeReferences(children.eq(8).text())
+		}
+		else if (!children.first().attr("colspan") && children.length == 2) {
+			launch.type += ', ' + removeReferences(children.eq(0).text())
+		}
+		else if (children.first().attr("colspan")) {
+			launch.note = removeReferences(children.eq(0).text())
+			launch.payloadIcon = launch.payloadIcon || getPayloadIcon(launch.note)
+			launches.push(launch)
+		}
+	})
+	return launches
 }
 
 function getPayloadIcon(text) {
